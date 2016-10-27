da2615710f82b8f2cbfa681c8a116fba8aa2ad15
Rayest <yi@rayest.cn>
2016-10-27 23:49:09 +0800

    shiro 实现登录的 servlet 配置

---
+++ src/main/java/cn/rayest/login/LoginServlet.java
@@ -0,0 +1,22 @@
+package cn.rayest.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Rayest on 2016/10/27 0027.
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
